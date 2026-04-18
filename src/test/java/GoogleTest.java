cfb7ff1b91488964c0c2aa8fdf271a00ec445025
Alan Campos <allancampos601@gmail.com>
2026-04-17 21:54:27 -0300

    Corrigindo YAML pipeline

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -3,15 +3,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleTest {
 
     @Test
+    @Description("Abrir Google e validar título")
     public void abrirGoogle() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // importante pro CI
-
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
         System.out.println("Título: " + driver.getTitle());
